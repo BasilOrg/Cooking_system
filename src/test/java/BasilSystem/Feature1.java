4e2d2c92172737849b060bbb79fc1a1fe9f8e8b0
Eng-BASIL BARAKAT <s12217132@stu.najah.edu>
2025-03-09 21:10:42 +0200

    first comment

--- src/test/java/BasilSystem/Feature1.java
+++ src/test/java/BasilSystem/Feature1.java
@@ -3,78 +3,128 @@ package BasilSystem;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.example.Customer;
+import org.example.TestInput;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Feature1 {
 
+    ArrayList<Customer>ac=new ArrayList<>();
 
-    @Given("I am a registered customer named {string}")
-    public void i_am_a_registered_customer_named(String string) {
+    @Then("the field {string} should be error")
+    public void theFieldShouldBeError(String string) {
+
+        System.out.println("Please enter the information");
+        assertEquals(false,false);
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("user clik on insert order and flag is {string}")
+    public void userClikOnInsertOrderAndFlagIs(String string) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("he fill in {string} with {string}")
+    public void heFillInWith(String string, String string2) {
+        boolean flag =false;
+        if(string.equals("Name")){
+
+            flag= TestInput.nameTest(string2);
+            if(flag) assertTrue(true);
+            else assertFalse(false);
+
+        }
+        if(string.equals("dietaryPresences")){
+
+            flag= TestInput.dietaryPresencesTest(string2);
+            if(flag) assertTrue(true);
+            else assertFalse(false);
+
+        }
+        if(string.equals("Allergies")){
+
+            flag= TestInput.allergiesTest(string2);
+            if(flag) assertTrue(true);
+            else assertFalse(false);
+
+        }
+
 
 
 
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @When("I navigate to the {string} section")
-    public void i_navigate_to_the_section(String string) {
+    @Then("the system should store {string} as a dietary preference and {string} for {string}")
+    public void theSystemShouldStoreAsADietaryPreferenceAndFor(String dietaryPresences, String allergies, String name) {
         // Write code here that turns the phrase above into concrete actions
-    }
+        Customer customer=new Customer();
+        customer.setName(name);
+        customer.setDietaryPreference(dietaryPresences);
+        customer.setAllergy(allergies);
 
-    @When("I enter {string} as my dietary preference")
-    public void i_enter_as_my_dietary_preference(String string) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @When("I enter {string} as an allergy")
-    public void i_enter_as_an_allergy(String string) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @When("I submit my preferences")
-    public void i_submit_my_preferences() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Then("the system should store {string} as a dietary preference for {string}")
-    public void the_system_should_store_as_a_dietary_preference_for(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Then("the system should store {string} as an allergy for {string}")
-    public void the_system_should_store_as_an_allergy_for(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
+        ac.add(customer);
     }
 
     @Then("I should receive a confirmation message that my information has been saved")
-    public void i_should_receive_a_confirmation_message_that_my_information_has_been_saved() {
+    public void iShouldReceiveAConfirmationMessageThatMyInformationHasBeenSaved() {
+
+        System.out.println("The information saved Successfully!");
+        assertTrue(true);
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Given("a customer {string} exists with dietary preferences {string} and allergies {string}")
-    public void a_customer_exists_with_dietary_preferences_and_allergies(String string, String string2, String string3) {
+    @When("user click on insert order and flag is {string}")
+    public void userClickOnInsertOrderAndFlagIs(String string) {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Given("I am logged in as a chef")
-    public void i_am_logged_in_as_a_chef() {
+    @Then("the user should see {string}")
+    public void theUserShouldSee(String string) {
+
+        boolean flag =false;
+
+            flag= TestInput.nameTest(string);
+            if(flag) assertTrue(true);
+            else System.out.println("Name is Invalid OR Name must be A a string ");
+
+
+
+
+
+            flag= TestInput.dietaryPresencesTest(string);
+            if(flag) assertTrue(true);
+            else  System.out.println("dietaryPresences must be a String  OR dietaryPresences is Invalid");
+
+
+
+
+            flag= TestInput.allergiesTest(string);
+            if(flag) assertTrue(true);
+            else ;System.out.println("Allergies must be a String  OR Allergies is Invalid   ");
+
+
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @When("I view the profile of customer {string}")
-    public void i_view_the_profile_of_customer(String string) {
+    @When("I retrieve the preferences for {string}")
+    public void iRetrieveThePreferencesFor(String string) {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Then("I should see {string} listed under dietary preferences")
-    public void i_should_see_listed_under_dietary_preferences(String string) {
+    @Then("I should see {string} in dietary preferences")
+    public void iShouldSeeInDietaryPreferences(String string) {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Then("I should see {string} listed under allergies")
-    public void i_should_see_listed_under_allergies(String string) {
+    @Then("I should see {string} in allergies")
+    public void iShouldSeeInAllergies(String string) {
         // Write code here that turns the phrase above into concrete actions
     }
-
 
 
 }
