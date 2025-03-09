28622e1b34c659bab7a9e301d48ccb9109d6045c
Eng-BASIL BARAKAT <s12217132@stu.najah.edu>
2025-03-09 20:39:58 +0200

    first comment

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Customer {
 
-
+    private  String dietaryPreference;
+    private  String name;
+    private  String allergy;
 
 
     public Customer() {
@@ -10,8 +12,41 @@ public class Customer {
     }
 
 
+    public void setName(String name){
 
-   }
+        this.name=name;
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+
+
+    public  void setDietaryPreference(String dietaryPreference){
+
+        this.dietaryPreference=dietaryPreference;
+    }
+
+    public  String getDietaryPreference(){
+
+        return dietaryPreference;
+    }
+
+
+
+
+    public  void  setAllergy(String allergy){
+
+        this.allergy=allergy;
+    }
+
+    public String getAllergy(){
+
+        return allergy;
+    }
+
 
 
 
